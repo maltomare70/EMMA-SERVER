e73171b3a122d23491e7efe3a54548957c29a17f
Marco <maltomare70@gmail.com>
2026-08-15 09:42:31 +0200

    Setup

---
+++ Emma.Services/Services/AdminServiceClient.cs
@@ -0,0 +1,35 @@
+﻿using Emma.Services.Http;
+using EmmaServer.Entities;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace Emma.Services.Services;
+
+public interface IAdminServiceClient
+{
+
+}
+public class AdminServiceClient : ServiceClientBase, IAdminServiceClient
+{
+    private const string EndpointInit = "/api/database/init";
+
+    public AdminServiceClient(string url, string user, string password)
+    : base(url, user, password)
+    {
+    }
+
+    public AdminServiceClient(HttpClient httpClient, string url, string user, string password)
+        : base(httpClient, url, user, password)
+    {
+    }
+
+    /// <inheritdoc />
+    protected override Exception CreateError(HttpResponseMessage response, string body)
+        => InvioError(response, body);
+
+
+    public async Task InitAsync()
+    => await PostAsync(EndpointInit).ConfigureAwait(false);
+}
